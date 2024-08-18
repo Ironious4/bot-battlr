0536c3e79e69baa599bbbd0d6aaf9e2e9c92eb7e
Ironious4 <kariukityrone525@gmail.com>
2024-08-18 11:24:44 +0300

    Update BotsPage.js

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -18,7 +18,7 @@ function BotsPage() {
   const handleBotDelete = async (botToDelete) => {
     try {
       // send delete request to the backend
-      await fetch(`http://localhost:3000/bots/${botToDelete.id}`, {
+      await fetch(`https://bot-battlr-eta-seven.vercel.app/bots/${botToDelete.id}`, {
         method: "DELETE",
       });
       // Remove the bot from the army if it's in the army
